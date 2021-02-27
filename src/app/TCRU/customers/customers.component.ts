a4ed8bf5d709f236771f3b21aec4083126b1c23e
Fahg2541 <Fahg2541@gmail.com>
2021-02-27 13:11:46 +0700

    edit customers

--- src/app/TCRU/customers/customers.component.ts
+++ src/app/TCRU/customers/customers.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, AfterViewInit, ViewChild} from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild, ElementRef, OnDestroy} from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
@@ -6,6 +6,9 @@ import { HttpClient } from '@angular/common/http';
 import { CustomersService } from '../../Service/customers.service';
 import { MatDialog } from '@angular/material/dialog';
 import { DialogCustomerComponent } from '../dialog-customer/dialog-customer.component';
+import { Router, ActivatedRoute } from '@angular/router';
+import { FormControl, FormGroup, FormBuilder, FormsModule, Validators } from '@angular/forms';
+import * as _ from 'lodash';
 
 export interface UserData {
   id: string;
@@ -19,13 +22,41 @@ export interface UserData {
   templateUrl: './customers.component.html',
   styleUrls: ['./customers.component.css']
 })
+
 export class CustomersComponent implements AfterViewInit {
 
+  private files: File[] = [];
+  private router: Router;
+  private uploadRoute: string = 'api/Customers';
+
+  @ViewChild('labelImport')
+  labelImport: ElementRef;
+  formImport: FormGroup;
+  fileToUpload: File = null;
+  fileUploadSub: any;
+  uploadingProgressing: boolean = false;
+  uploadProgress: number = 0;
+  uploadComplete: boolean = false;
+  serverResponse: any;
+
+  customersList: any[];
+  customer_id: string;
+  customersMessage: string;
+  fileName = 'Customers.xlsx';
+  searchText: string;
+  term: string;
+  filterTerm: string;
+  page:any = 1;
+  limit: any = 10;
+  skip: any;
+  categoryArr:any;
+  totalCount: any;
   dataArr: any;
 
   constructor(private http: HttpClient,
     private Customers: CustomersService,
-    public dialog: MatDialog) { }
+    public dialog: MatDialog,
+    private route: ActivatedRoute,) { }
 
     openDialog(){
       let dialogRef = this.dialog.open(DialogCustomerComponent);
@@ -55,11 +86,11 @@ export class CustomersComponent implements AfterViewInit {
     }
   }
 
-  getCustomer()
-  {
-    this.Customers.getData().subscribe(res=> {
-      this.dataArr = res;
-    })
-  }
+  // getCustomer()
+  // {
+  //   this.Customers.getData1().subscribe(res=> {
+  //     this.dataArr = res;
+  //   })
+  // }
 
 }
